1e728d4b13bffb5750cdda18561bfb2f52eaa385
Eyyan Amir <ayyanamir6@gmail.com>
2021-12-09 14:54:33 +0500

    new updates

--- src/App.js
+++ src/App.js
@@ -13,16 +13,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className='para'>
+        <p className=''>
           Edit <code>src/App.js</code> and save to reloads.
         </p>
         <a
-			className="App-link"
-			href="https://reactjs.org"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			Learn React
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
         <Main />
       </header>
