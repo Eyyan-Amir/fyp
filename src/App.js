dffeec86159ec2dda1d09eef736dfa4143cdf08a
Eyyan-Amir <ayyanamir6@gmail.com>
2021-12-11 15:52:43 +0500

    new commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import React from "react";
 import "./assets/scss/styles.scss";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
-import Main from './components/Main'
-
-
+import Main from "./components/Main";
 
 function App() {
-  return (
-    <>
-		<div className="App">
-        	<Header />
-          		<Main />
-        	<Footer />
-    	</div>
-    </>
-  );
+	return (
+		<>
+			<div className='App'>
+				<Header />
+				<Main />
+				<Footer />
+			</div>
+		</>
+	);
 }
 
 export default App;
-
